88284eda844f3b5b1a19b20a9c66aa5b03df4e9f
Royal Finch <Royal2791@gmail.com>
2024-05-08 15:59:38 -0400

    add initial html structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,36 @@
-export default function App() {}
+export default function App() {
+  return (
+    <>
+      <main className="main">
+        <img src="/images/logo.svg" alt="logo" className="logo" />
+        <h1>
+          We are launching <span>soon!</span>
+        </h1>
+        <p>Subscribe and get notified</p>
+        <form action="" className="subscribe-form">
+          <input
+            type="text"
+            name="email"
+            id="email"
+            className="subscribe-form__input-email"
+          />
+          <button className="subscribe-form__submit">Notify Me</button>
+        </form>
+        <img
+          src="/images/illustration-dashboard.png"
+          alt="dashboard illustration"
+        />
+      </main>
+      <footer>
+        <ul className="social-media-links">
+          <li className="social-media-link"></li>
+          <li className="social-media-link"></li>
+          <li className="social-media-link"></li>
+        </ul>
+        <span className="copyright">
+          &copy; Copyright Ping. All rights reserved.
+        </span>
+      </footer>
+    </>
+  );
+}
