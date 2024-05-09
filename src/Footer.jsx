5cc5585e2ed3d3dd8a08131b5f31d940247ca1e4
Royal Finch <Royal2791@gmail.com>
2024-05-09 19:02:25 -0400

    fix footer accessibility
    - wrap social media icons in anchor tags
    - add screen reader labels to social media links

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,23 +2,29 @@ export default function Footer() {
   return (
     <footer className="footer">
       <ul className="social-media-links">
-        <li className="social-media-link">
-          <i
-            className="fa-brands fa-facebook-f fa-xs"
-            style={{ color: '#4f7df3' }}
-          ></i>
+        <li>
+          <a href="#" className="social-media-link" aria-label="facebook">
+            <i
+              className="fa-brands fa-facebook-f fa-xs"
+              style={{ color: '#4f7df3' }}
+            ></i>
+          </a>
         </li>
-        <li className="social-media-link">
-          <i
-            className="fa-brands fa-twitter fa-xs"
-            style={{ color: '#4f7df3' }}
-          ></i>
+        <li>
+          <a href="#" className="social-media-link" aria-label="twitter">
+            <i
+              className="fa-brands fa-twitter fa-xs"
+              style={{ color: '#4f7df3' }}
+            ></i>
+          </a>
         </li>
-        <li className="social-media-link">
-          <i
-            className="fa-brands fa-instagram fa-xs"
-            style={{ color: '#4f7df3' }}
-          ></i>
+        <li>
+          <a className="social-media-link" href="#" aria-label="instagram">
+            <i
+              className="fa-brands fa-instagram fa-xs"
+              style={{ color: '#4f7df3' }}
+            ></i>
+          </a>
         </li>
       </ul>
       <span className="copyright">
